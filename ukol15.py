930b006045328735b6fcf1af02bf79cde53fef84
Pavla Adéeová <adeeova@gmail.com>
2018-10-28 16:12:38 +0100

    pridana nefunkcni funkce na vyhledavani slov dle pismen v retezci

---
+++ ukol15.py
@@ -0,0 +1,10 @@
+def soucet(a, b, c):
+    scitani = a + b + c
+    if scitani//2 == 1:
+        return sude
+
+a = float(input('zadej první číslo'))
+b = float(input('zadej druhé číslo'))
+c = float(input('zadej třetí číslo'))
+soucet(a, b, b)
+print(soucet)
