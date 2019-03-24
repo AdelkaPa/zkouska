0090d7f7afa7619997b5e7a88b405e2d1ea8c292
Pavla Adéeová <adeeova@gmail.com>
2019-03-24 21:08:35 +0100

    uprava mandaly
    zmeneny uhly, ale stale asymetrie

--- kvetina.py
+++ kvetina.py
@@ -2,21 +2,31 @@ import turtle
 from turtle import *
 turtle.speed(9)
 color('red', 'grey')
+n = 100
+
 begin_fill()
 while True:
-    forward(200)
+    forward(2*n)
     left(170)
     if abs(pos()) < 1:
         break
+left(10)
+penup()
+forward(n)
+pendown()
 
-for i in range(4):
-    right(30)
-    for i in range(3):
-        forward(200)
-        for i in range(35):
-            forward(100)
-            left(170)
-        #right(10)
+for i in range(9):
+    penup()
+    forward(n)
+    pendown()
+    for i in range(35):
+        forward(n)
+        left(170)
+    penup()
+    left(10)
+    forward(2*n)
+    pendown()
+        #righ(10)
     #forward(100)
 
 end_fill()
